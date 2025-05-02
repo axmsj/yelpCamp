8467ab1d1445d241185c6e8ded256bc9bd627415
Armando Gutierrez <115781553+axmsj@users.noreply.github.com.>
2025-05-02 01:22:35 -0400

    CRUD Funcitonality Complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 async function main() {
@@ -12,6 +13,8 @@ main().catch((err) => console.log('OH NO MONGO CONNECTION ERROR!!!', err));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -27,11 +30,39 @@ app.get('/campgrounds', async (req, res) => {
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new');
 });
+
+//POST SUBMIT FORM DATA DATA
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
 //Show ejs Specific id - Route
 app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/show', { campground });
+});
+
+//GET - View Edit Page
+app.get('/campgrounds/:id/edit', async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
-  res.render('campgrounds/show', { campground });
+  res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, req.body.campground, { new: true });
+  console.log(campground);
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const deleted = await Campground.findByIdAndDelete(id);
+  console.log(deleted);
+  res.redirect('/campgrounds');
 });
 
 app.listen(3000, () => {
